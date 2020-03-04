c0e79c4ccb203c3ab5c2fb989dbfab4df75b1856
David312 <davidhs312yt@gmail.com>
2020-03-04 20:34:40 +0100

    Create Tiles classes

---
+++ core/src/main/kotlin/com/github/david312/tictactoe/kotlin/core/domain/Tiles.kt
@@ -0,0 +1,29 @@
+package com.github.david312.tictactoe.kotlin.core.domain
+
+/**
+ * Possible values that a tile can have.
+ * @param symbol the string representing its value.
+ */
+enum class TileValue(val symbol: String) { TIC("X"), TAC("O"), EMPTY(" ") }
+
+/**
+ * Class representing a single tile.
+ */
+sealed class Tile(private val value: TileValue) {
+    override fun toString(): String = value.symbol
+}
+
+/**
+ * A Tile marked with a TIC value.
+ */
+class TicTile : Tile(TileValue.TIC)
+
+/**
+ * A Tile marked with a TAC value.
+ */
+class TacTile : Tile(TileValue.TAC)
+
+/**
+ * A Tile marked with no value.
+ */
+class EmptyTile : Tile(TileValue.EMPTY)
